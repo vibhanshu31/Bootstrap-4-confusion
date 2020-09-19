ca3a3632b8dadd55841044503b6b05853f247e4b
Vibhanshu Shandil <vibhanshushandil31@gmail.com>
2020-09-19 20:57:37 +0530

    Updated all dependencies, Resolved security issues and made ready for production

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
-
-$(document).ready(function(){
-    $("#mycarousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
-        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
-            $("#mycarousel").carousel('pause');
-            $("#carouselButton").children("span").removeClass('fa-pause');
-            $("#carouselButton").children("span").addClass('fa-play');
-        }
-        else {
-            $("#mycarousel").carousel('cycle');
-            $("#carouselButton").children("span").removeClass('fa-play');
-            $("#carouselButton").children("span").addClass('fa-pause');
-        }
-    });
+$(document).ready(function () {
+	$("#mycarousel").carousel({ interval: 2000 });
+	$("#carouselButton").click(function () {
+		if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+			$("#mycarousel").carousel("pause");
+			$("#carouselButton").children("span").removeClass("fa-pause");
+			$("#carouselButton").children("span").addClass("fa-play");
+		} else {
+			$("#mycarousel").carousel("cycle");
+			$("#carouselButton").children("span").removeClass("fa-play");
+			$("#carouselButton").children("span").addClass("fa-pause");
+		}
+	});
 });
 
-$("#loginButton").click(function() {
-    $("#loginModal").modal();
+$("#loginButton").click(function () {
+	$("#loginModal").modal();
 });
 
-$("#reserveButton").click(function() {
-    $("#reservationModal").modal('show');
+$("#reserveButton").click(function () {
+	$("#reservationModal").modal("show");
+});
+
+$(".submit-btn").click(function () {
+	alert("Thank you For your Feedback");
 });
